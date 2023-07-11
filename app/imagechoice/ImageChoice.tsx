4bff3442fa30e2450daf6d8e6284b098fcaadab2
farhanakhtar24 <akhtar.farhan779@gmail.com>
2023-07-11 15:27:58 +0530

    fetching images from picsum api

--- app/imagechoice/ImageChoice.tsx
+++ app/imagechoice/ImageChoice.tsx
@@ -1,23 +1,48 @@
 "use client";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { ImageData } from "./ImagesData";
 
 type Props = {};
 
 const ImageChoice = (props: Props) => {
-	const [users, setUsers] = useState([]);
+	const [images, setImages] = useState<ImageData[]>([]);
+
 	useEffect(() => {
-		async () => {
-			// const response = await fetch("https://api.pexels.com/v1/");
-			// const users = await response.json();
-			setUsers(users);
+		// fetch images
+		const fetchImages = async () => {
+			const res = await fetch("https://picsum.photos/v2/list?limit=8");
+			const data = await res.json();
+			// convert data to ImageData type
+			const imagesData: ImageData[] = data.map((image: any) => {
+				return {
+					id: image.id,
+					alt: image.author,
+					url: image.download_url,
+					correct: true,
+				};
+			});
+			setImages(imagesData);
+			console.log("data", imagesData);
 		};
-	});
+		fetchImages();
+	}, []);
+
 	return (
 		<div className="w-full h-full flex flex-col p-12 gap-5">
-			<div>Which of these is a human being ?</div>
-			<div>
-				{users.map((user, idx) => {
-					return <div key={idx}>use</div>;
+			<div>Which of these is a laptop ?</div>
+			<div className="w-full h-full grid grid-cols-4 grid-rows-2 gap-5">
+				{images?.map((image, idx) => {
+					return (
+						<Image
+							className="w-full h-full"
+							key={idx}
+							src={image.url}
+							alt="user"
+							width={999}
+							height={999}
+						/>
+					);
 				})}
 			</div>
 		</div>
---
+++ app/imagechoice/ImagesData.ts
@@ -0,0 +1,6 @@
+export interface ImageData {
+	id: number;
+	alt: string;
+	url: string;
+	correct: boolean;
+}
