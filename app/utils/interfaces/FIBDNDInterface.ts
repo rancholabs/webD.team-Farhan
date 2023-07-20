df9dcf29b79fd8725f78f6c834515effba03b5fc
farhanakhtar24 <akhtar.farhan779@gmail.com>
2023-07-20 16:22:52 +0530

    done

---
+++ app/utils/interfaces/FIBDNDInterface.ts
@@ -0,0 +1,22 @@
+export interface DroppableItemInterface {
+	index: number;
+	name: string;
+}
+
+export interface FIBDNDDataInterface {
+	id: number;
+	question: (string | null)[];
+	answers: {
+		index: number;
+		answer: string;
+	}[];
+	submittedAnswers: {
+		index: number;
+		answer: string;
+	}[];
+	validationFIB: {
+		score: number;
+		correct: number;
+		wrong: number;
+	};
+}
