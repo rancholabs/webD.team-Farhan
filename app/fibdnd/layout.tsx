4e26c691bf1532d52f12834aad0db0237eab7b5d
farhanakhtar24 <akhtar.farhan779@gmail.com>
2023-07-22 13:48:09 +0530

    bug fixes

--- app/fibdnd/layout.tsx
+++ app/fibdnd/layout.tsx
@@ -1,4 +1,4 @@
-export default function h5pLayout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<section className="w-full h-full overflow-hidden">{children}</section>
 	);
--- app/flashcard/layout.tsx
+++ app/flashcard/layout.tsx
@@ -1,3 +1,3 @@
-export default function h5pLayout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
 	return <section className="w-full h-full">{children}</section>;
 }
